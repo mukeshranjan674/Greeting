6dc7d38cea38ee1dd52ac31a333a740fdf1ec331
mukeshranjan674 <mukeshranjan674@gmail.com>
2025-08-18 22:46:26 +0530

    Initial commit

---
+++ src/main/java/com/example/greeting/controllers/GreetingController.java
@@ -0,0 +1,19 @@
+package com.example.greeting.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1")
+public class GreetingController {
+	
+	@GetMapping("/get")
+	public ResponseEntity<String> get(){
+		String greeting = "Hello";
+		return new ResponseEntity<>(greeting, HttpStatus.OK);
+	}
+
+}
